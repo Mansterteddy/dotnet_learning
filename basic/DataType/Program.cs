878ef44c9e7f39c82bc8578e959bcd85b68dcbfd
mansterteddy <mansterteddy@gmail.com>
2021-05-31 23:32:38 +0800

    Add DataType.

---
+++ basic/DataType/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Size of int: {0}, size of float: {1}, size of double: {2}", sizeof(int), sizeof(float), sizeof(double));
+            Console.ReadLine();
+        }
+    }
+}
