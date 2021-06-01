932f576d7c70ca9c19d850b6015f7c59f5c14d0a
mansterteddy <mansterteddy@gmail.com>
2021-06-01 23:22:38 +0800

    Add TypeConversion.

---
+++ basic/TypeConversion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            double d = 5673.74;
+            int i;
+
+            i = (int)d;
+            Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}
